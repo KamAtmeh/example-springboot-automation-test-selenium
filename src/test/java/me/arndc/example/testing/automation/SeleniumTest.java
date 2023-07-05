29d43469bcdbd1dabf4ba6d1b00ed543388344b7
KamAtmeh <75306898+KamAtmeh@users.noreply.github.com>
2023-07-05 16:38:35 +0200

    Update SeleniumTest.java

--- src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
+++ src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
@@ -37,6 +37,7 @@ public class SeleniumTest {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         
     }
